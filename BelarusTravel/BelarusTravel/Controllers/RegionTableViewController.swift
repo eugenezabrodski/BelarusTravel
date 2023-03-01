e097cc7b75e44eabd518b0d7a81993771c68c38d
Eugene <115879896+eugenezabrodski@users.noreply.github.com>
2023-03-01 14:33:41 +0300

    01.03.2023

--- BelarusTravel/BelarusTravel/Controllers/RegionTableViewController.swift
+++ BelarusTravel/BelarusTravel/Controllers/RegionTableViewController.swift
@@ -11,21 +11,14 @@ import FirebaseStorage
 
 class RegionTableViewController: UITableViewController {
     
-    var arrayOfRegion = [Region]()
+    var regions: [Region] = []
     let headerID = String(describing: CustomHeaderView.self)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchUsers()
         tableViewConfig()
         tableView.backgroundView = UIImageView(image: #imageLiteral(resourceName: "MEANWHILE"))
-        arrayOfRegion = [
-            Region(region: "Бресткая область", typeTravel: ["архитектура", "природа", "музеи"], isExpanded: false),
-            Region(region: "Минская область", typeTravel: ["архитектура", "природа", "музеи"], isExpanded: false),
-            Region(region: "Витебская область", typeTravel: ["архитектура", "природа", "музеи"], isExpanded: false),
-            Region(region: "Гомельская область", typeTravel: ["архитектура", "природа", "музеи"], isExpanded: false),
-            Region(region: "Гродненская область", typeTravel: ["архитектура", "природа", "музеи"], isExpanded: false),
-            Region(region: "Могилевская область", typeTravel: ["архитектура", "природа", "музеи"], isExpanded: false)
-        ]
          
     }
     
@@ -42,28 +35,28 @@ class RegionTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        arrayOfRegion.count
+        regions.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if !arrayOfRegion[section].isExpanded {
+        if !regions[section].isExpanded! {
                     return 0
                 }
-        return arrayOfRegion[section].typeTravel.count
+        return regions[section].regionId
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = arrayOfRegion[indexPath.section].typeTravel[indexPath.row]
+        cell.textLabel?.text = regions[indexPath.section].typeId
         cell.backgroundView = UIImageView(image: #imageLiteral(resourceName: "backgroundcell"))
         return cell
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerID) as! CustomHeaderView
-        header.configure(title: arrayOfRegion[section].region, section: section)
-        header.rotateImage(arrayOfRegion[section].isExpanded)
+        header.configure(title: regions[section].nameRegion!, section: section)
+        header.rotateImage(regions[section].isExpanded!)
         header.delegate = self
         return header
         }
@@ -79,6 +72,28 @@ class RegionTableViewController: UITableViewController {
             tableView.tableFooterView = UIView()
         }
 
+    
+    private func fetchUsers() {
+        guard let url = URL(string: "http://localhost:3000/travel") else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            if let error = error {
+                print(error)
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                self?.regions = try JSONDecoder().decode([Region].self, from: data)
+            } catch {
+                print (error)
+            }
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        task.resume()
+    }
 
     /*
     // MARK: - Navigation
@@ -96,8 +111,8 @@ extension RegionTableViewController: HeaderViewDelegate {
 func expandedSection(button: UIButton) {
 let section = button.tag
 
-let isExpanded = arrayOfRegion[section].isExpanded
-    arrayOfRegion[section].isExpanded = !isExpanded
+let isExpanded = regions[section].isExpanded
+    regions[section].isExpanded = !isExpanded!
 
 tableView.reloadSections(IndexSet(integer: section), with: .automatic)
 }
--- BelarusTravel/BelarusTravel/Models/RegionModel.swift
+++ BelarusTravel/BelarusTravel/Models/RegionModel.swift
@@ -5,11 +5,21 @@
 //  Created by Евгений Забродский on 6.02.23.
 //
 
-import Foundation
+import UIKit
 
-struct Region {
-    var region: String
-    var typeTravel: [String]
-    var isExpanded: Bool
+struct Region: Codable {
+    var regionId: Int
+    var nameRegion: String?
+    var typeId: String?
+    var placeId: String?
+    var text: String?
+    var coordinates: Coordinates?
+    var photoURL: String?
+    var isExpanded: Bool?
+}
+
+struct Coordinates: Codable {
+    let lat: String?
+    let lng: String?
 }
 
